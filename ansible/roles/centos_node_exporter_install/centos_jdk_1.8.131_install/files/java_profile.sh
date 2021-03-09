2083bab706ceff710c5a378dbc22625ab3d4b7c5
felix-fy <58977279+felix-fy@users.noreply.github.com>
2021-03-09 12:49:37 +0800

    Create java_profile.sh

---
+++ ansible/roles/centos_node_exporter_install/centos_jdk_1.8.131_install/files/java_profile.sh
@@ -0,0 +1,5 @@
+JAVA_HOME=/usr/local/java/jdk/jdk1.8.0_131
+JRE_HOME=/usr/local/java/jdk/jdk1.8.0_131/jre
+CLASS_PATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar:$JRE_HOME/lib
+PATH=$PATH:$JAVA_HOME/bin:$JRE_HOME/bin
+export JAVA_HOME JRE_HOME CLASS_PATH PATH
