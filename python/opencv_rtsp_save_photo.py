d69001f179ff1c9c7bfd4d5fa893e050aaffdfa6
felix-fy <58977279+felix-fy@users.noreply.github.com>
2021-04-12 23:06:58 +0800

    Update opencv_rtsp_save_photo.py

--- python/opencv_rtsp_save_photo.py
+++ python/opencv_rtsp_save_photo.py
@@ -7,12 +7,16 @@ path_file = './source_file/'
 cap = cv2.VideoCapture('rtsp://admin:1234568899@10.10.150.119:554/h264/ch2/main/av_stream')
 flag = 0
 
-while (cap.isOpened()):
-    cap.set(cv2.CAP_PROP_POS_FRAMES,flag) #设置帧数标记
-    ret,im = cap.read()#get photo
-    #cv2.waitKey(2000)#延时
-    cv2.imwrite(path_file+'{}.jpg'.format(flag),im)#save photo
-    flag+=1#set photo number
-    time.sleep( 3 )
-    if not ret:
-        break
+def main ():
+    while (cap.isOpened()):
+        cap.set(cv2.CAP_PROP_POS_FRAMES,flag) #设置帧数标记
+        ret,im = cap.read()#get photo
+        #cv2.waitKey(2000)#延时
+        cv2.imwrite(path_file+'{}.jpg'.format(flag),im)#save photo
+        flag+=1#set photo number
+        time.sleep( 3 )
+        if not ret:
+            break
+            
+if __name__ == '__main__':
+    main()
